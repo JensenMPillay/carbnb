77c5996346b3b044132b6460ad1668c228596e3e
JensenMPillay <jensen_pillay@hotmail.fr>
2024-01-04 17:09:14 +0100

    [Chore] Sync Prisma/Supabase

---
+++ prisma/migrations/init/migration.sql
@@ -0,0 +1,158 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "hstore";
+
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "pg_trgm";
+
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "postgis";
+
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('ADMIN', 'LENDER', 'RENTER');
+
+-- CreateEnum
+CREATE TYPE "public"."Category" AS ENUM ('COMPACT', 'LUXURY', 'SEDAN', 'SUV', 'SPORT');
+
+-- CreateEnum
+CREATE TYPE "public"."Transmission" AS ENUM ('AUTOMATIC', 'MANUAL');
+
+-- CreateEnum
+CREATE TYPE "public"."FuelType" AS ENUM ('DIESEL', 'ELECTRIC', 'HYBRID', 'PETROL');
+
+-- CreateEnum
+CREATE TYPE "public"."PaymentStatus" AS ENUM ('PENDING', 'PROCESSING', 'SUCCEEDED', 'FAILED', 'CANCELED', 'REFUNDED');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" TIMESTAMP(3),
+    "stripeCustomerId" TEXT,
+    "role" "public"."Role" NOT NULL DEFAULT 'RENTER',
+    "name" TEXT,
+    "phone" TEXT,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Session" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "accessToken" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Car" (
+    "id" SERIAL NOT NULL,
+    "category" "public"."Category" NOT NULL,
+    "model" TEXT NOT NULL,
+    "year" INTEGER,
+    "transmission" "public"."Transmission" NOT NULL,
+    "fuelType" "public"."FuelType" NOT NULL,
+    "pricePerDay" DOUBLE PRECISION NOT NULL,
+    "available" BOOLEAN NOT NULL,
+    "userId" TEXT NOT NULL,
+    "locationId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Car_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Booking" (
+    "id" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "totalPrice" DOUBLE PRECISION NOT NULL,
+    "paymentStatus" "public"."PaymentStatus" NOT NULL,
+    "stripePaymentId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "carId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Location" (
+    "id" TEXT NOT NULL,
+    "latitude" DOUBLE PRECISION,
+    "longitude" DOUBLE PRECISION,
+    "address" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "public"."User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "public"."Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_accessToken_key" ON "public"."Session"("accessToken");
+
+-- CreateIndex
+CREATE INDEX "Car_userId_idx" ON "public"."Car"("userId");
+
+-- CreateIndex
+CREATE INDEX "Car_locationId_idx" ON "public"."Car"("locationId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Booking_id_key" ON "public"."Booking"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Booking_stripePaymentId_key" ON "public"."Booking"("stripePaymentId");
+
+-- CreateIndex
+CREATE INDEX "Booking_startDate_endDate_idx" ON "public"."Booking"("startDate", "endDate");
+
+-- CreateIndex
+CREATE INDEX "Booking_userId_idx" ON "public"."Booking"("userId");
+
+-- CreateIndex
+CREATE INDEX "Booking_carId_idx" ON "public"."Booking"("carId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Location_id_key" ON "public"."Location"("id");
+
+-- CreateIndex
+CREATE INDEX "Location_latitude_longitude_idx" ON "public"."Location"("latitude", "longitude");
+
+-- AddForeignKey
+ALTER TABLE "public"."Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Car" ADD CONSTRAINT "Car_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Car" ADD CONSTRAINT "Car_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "public"."Location"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Booking" ADD CONSTRAINT "Booking_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "public"."Booking" ADD CONSTRAINT "Booking_carId_fkey" FOREIGN KEY ("carId") REFERENCES "public"."Car"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
