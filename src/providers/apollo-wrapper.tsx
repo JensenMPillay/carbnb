63fdea60678bce6a2048f722ec75964889fffe58
JensenMPillay <jensen_pillay@hotmail.fr>
2024-01-05 15:42:15 +0100

    [Features] Add Appolo Client

---
+++ src/providers/apollo-wrapper.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRApolloClient,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+import { absoluteUrl } from "../lib/utils";
+
+// Create Client
+function makeClient() {
+  const httpLink = new HttpLink({
+    // Absolute URL ! (SSR)
+    uri: absoluteUrl("/api/graphql"),
+    fetchOptions: { cache: "no-store" },
+    // you can override the default `fetchOptions` on a per query basis
+    // via the `context` property on the options passed as a second argument
+    // to an Apollo Client data fetching hook, e.g.:
+    // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            // in a SSR environment, if you use multipart features like
+            // @defer, you need to decide how to handle these.
+            // This strips all interfaces with a `@defer` directive from your queries.
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+// Component Wrapper
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
