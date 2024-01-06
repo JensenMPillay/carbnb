9c40d794a27fa2ddbc90fcbfa0f78afc43b53189
JensenMPillay <jensen_pillay@hotmail.fr>
2024-01-06 17:08:07 +0100

    [Features][Back] Add CRUD Car & Location

--- src/app/api/graphql/types/Car.ts
+++ src/app/api/graphql/types/Car.ts
@@ -1,4 +1,6 @@
+import prisma from "@/prisma/prisma";
 import { Category, FuelType, Transmission } from "@prisma/client";
+import { createGraphQLError } from "graphql-yoga";
 import { builder } from "../builder";
 
 // Category
@@ -33,3 +35,204 @@ builder.prismaObject("Car", {
     createdAt: t.expose("createdAt", { type: "Date" }),
   }),
 });
+
+// GET Route
+builder.queryField("getCar", (t) =>
+  t.prismaField({
+    type: "Car",
+    args: {
+      id: t.arg.int({ required: true }),
+    },
+    resolve: async (query, _parent, args, ctx) => {
+      if (!(await ctx).user)
+        throw createGraphQLError(
+          "You have to be logged in to perform this action.",
+        );
+
+      const dbCar = await prisma.car.findUnique({
+        ...query,
+        where: {
+          id: args.id,
+        },
+      });
+
+      if (!dbCar) throw createGraphQLError("Car does not exist.");
+
+      return dbCar;
+    },
+  }),
+);
+
+// POST Route
+builder.mutationField("registerCar", (t) =>
+  t.prismaField({
+    type: "Car",
+    args: {
+      category: t.arg({ type: Category, required: true }),
+      model: t.arg.string({ required: true }),
+      year: t.arg.int({ required: false }),
+      transmission: t.arg({ type: Transmission, required: true }),
+      fuelType: t.arg({ type: FuelType, required: true }),
+      pricePerDay: t.arg.float({ required: true }),
+      available: t.arg.boolean({ required: false }),
+      locationId: t.arg.string({ required: true }),
+    },
+    resolve: async (query, _parent, args, ctx) => {
+      if (!(await ctx).user)
+        throw createGraphQLError(
+          "You have to be logged in to perform this action.",
+        );
+
+      const dbUser = await prisma.user.findUnique({
+        ...query,
+        where: {
+          id: (await ctx).user?.id,
+        },
+      });
+
+      if (!dbUser) throw createGraphQLError("User does not exists.");
+
+      const dbLocation = await prisma.location.findUnique({
+        where: {
+          id: args.locationId,
+        },
+      });
+
+      if (!dbLocation) throw createGraphQLError("Location does not exists.");
+
+      const carPrisma = await prisma.car.create({
+        ...query,
+        data: {
+          category: args.category,
+          model: args.model,
+          year: args.year ?? undefined,
+          transmission: args.transmission,
+          fuelType: args.fuelType,
+          pricePerDay: args.pricePerDay,
+          available: args.available ?? false,
+          userId: dbUser.id,
+          locationId: dbLocation.id,
+        },
+      });
+
+      if (!carPrisma)
+        throw createGraphQLError(
+          "An error occurred while updating the car information.",
+        );
+
+      return carPrisma;
+    },
+  }),
+);
+
+// POST Route
+builder.mutationField("updateCar", (t) =>
+  t.prismaField({
+    type: "Car",
+    args: {
+      id: t.arg.int({ required: true }),
+      category: t.arg({ type: Category, required: false }),
+      model: t.arg.string({ required: false }),
+      year: t.arg.int({ required: false }),
+      transmission: t.arg({ type: Transmission, required: false }),
+      fuelType: t.arg({ type: FuelType, required: false }),
+      pricePerDay: t.arg.float({ required: false }),
+      available: t.arg.boolean({ required: false }),
+      locationId: t.arg.string({ required: false }),
+    },
+    resolve: async (query, _parent, args, ctx) => {
+      if (!(await ctx).user)
+        throw createGraphQLError(
+          "You have to be logged in to perform this action.",
+        );
+
+      const dbUser = await prisma.user.findUnique({
+        ...query,
+        where: {
+          id: (await ctx).user?.id,
+        },
+      });
+
+      if (!dbUser) throw createGraphQLError("User does not exist.");
+
+      const dbCar = await prisma.car.findUnique({
+        ...query,
+        where: {
+          id: args.id,
+        },
+      });
+
+      if (!dbCar) throw createGraphQLError("Car does not exist.");
+
+      const dbLocation = args.locationId
+        ? await prisma.location.findUnique({
+            where: {
+              id: args.locationId,
+            },
+          })
+        : null;
+
+      const carPrisma = await prisma.car.update({
+        ...query,
+        where: {
+          id: args.id,
+        },
+        data: {
+          category: args.category ?? undefined,
+          model: args.model ?? undefined,
+          year: args.year ?? undefined,
+          transmission: args.transmission ?? undefined,
+          fuelType: args.fuelType ?? undefined,
+          pricePerDay: args.pricePerDay ?? undefined,
+          available: args.available ?? undefined,
+          userId: dbUser.id,
+          locationId: dbLocation?.id ?? undefined,
+        },
+      });
+
+      if (!carPrisma)
+        throw createGraphQLError(
+          "An error occurred while updating the car information.",
+        );
+
+      return carPrisma;
+    },
+  }),
+);
+
+// POST Route
+builder.mutationField("deleteUser", (t) =>
+  t.prismaField({
+    type: "Car",
+    args: {
+      id: t.arg.int({ required: true }),
+    },
+    resolve: async (query, _parent, args, ctx) => {
+      if (!(await ctx).user)
+        throw createGraphQLError(
+          "You have to be logged in to perform this action.",
+        );
+
+      const dbUser = await prisma.user.findUnique({
+        ...query,
+        where: {
+          id: (await ctx).user?.id,
+        },
+      });
+
+      if (!dbUser) throw createGraphQLError("User does not exist.");
+
+      const carPrisma = await prisma.car.delete({
+        ...query,
+        where: {
+          id: args.id,
+        },
+      });
+
+      if (!carPrisma)
+        throw createGraphQLError("An error occurred while deleting the car.");
+
+      return carPrisma;
+    },
+  }),
+);
--- src/app/api/graphql/types/Location.ts
+++ src/app/api/graphql/types/Location.ts
@@ -1,12 +1,71 @@
+import prisma from "@/prisma/prisma";
+import { createGraphQLError } from "graphql-yoga";
 import { builder } from "../builder";
 
 // Location Object
 builder.prismaObject("Location", {
   fields: (t) => ({
     id: t.exposeID("id"),
+    latitude: t.exposeFloat("latitude", { nullable: true }),
+    longitude: t.exposeFloat("longitude", { nullable: true }),
     address: t.exposeString("address"),
     city: t.exposeString("city"),
     country: t.exposeString("country"),
     cars: t.relation("cars"),
   }),
 });
+
+// GET Route
+builder.queryField("getLocation", (t) =>
+  t.prismaField({
+    type: "Location",
+    args: {
+      id: t.arg.string({ required: true }),
+    },
+    resolve: async (query, _parent, args, ctx) => {
+      const dbLocation = await prisma.location.findUnique({
+        ...query,
+        where: {
+          id: args.id,
+        },
+      });
+
+      if (!dbLocation) throw createGraphQLError("Location does not exist.");
+
+      return dbLocation;
+    },
+  }),
+);
+
+// POST Route
+builder.mutationField("registerLocation", (t) =>
+  t.prismaField({
+    type: "Location",
+    args: {
+      latitude: t.arg.float({ required: true }),
+      longitude: t.arg.float({ required: true }),
+      address: t.arg.string({ required: true }),
+      city: t.arg.string({ required: true }),
+      country: t.arg.string({ required: true }),
+    },
+    resolve: async (query, _parent, args, ctx) => {
+      const locationPrisma = await prisma.location.create({
+        ...query,
+        data: {
+          latitude: args.latitude,
+          longitude: args.longitude,
+          address: args.address,
+          city: args.city,
+          country: args.country,
+        },
+      });
+
+      if (!locationPrisma)
+        throw createGraphQLError(
+          "An error occurred while updating the location information.",
+        );
+
+      return locationPrisma;
+    },
+  }),
+);
