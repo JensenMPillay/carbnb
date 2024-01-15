b2141b5c59f4e5822855591372292632bf08a499
JensenMPillay <jensen_pillay@hotmail.fr>
2024-01-15 10:03:47 +0100

    [Fix] Scroll Behavior (min-h)

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -8,15 +8,13 @@ export default function AuthLayout({
 }) {
   return (
     // Main
-    <main className="flex h-[calc(100vh-9rem)] flex-row">
+    <main className="flex min-h-[calc(100vh-9rem)] flex-row">
       {/* Image  */}
       <aside className="relative hidden h-auto flex-1 md:flex">
         <RandomSVGComponent className="w-full" />
       </aside>
-      <section className="max-h-fit flex-1 px-8 md:px-10 lg:px-12">
-        <Card className="h-full overflow-auto py-4 md:py-8 lg:py-12">
-          {children}
-        </Card>
+      <section className="min-h-fit flex-1 px-8 md:px-10 lg:px-12">
+        <Card className="min-h-fit py-4 md:py-8 lg:py-12">{children}</Card>
       </section>
     </main>
   );
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,9 +7,9 @@ export default function DashboardLayout({
 }) {
   return (
     // Main
-    <main className="flex h-[calc(100vh-9rem)] flex-row">
-      <section className="max-h-fit w-full flex-1 px-8 md:px-10 lg:px-12">
-        <Card className="h-full flex-1 overflow-auto py-4">{children}</Card>
+    <main className="flex min-h-[calc(100vh-9rem)] flex-row">
+      <section className="min-h-fit w-full flex-1 px-8 md:px-10 lg:px-12">
+        <Card className="min-h-fit flex-1 py-4">{children}</Card>
       </section>
     </main>
   );
