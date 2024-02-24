d4a01997247bfb234595972682c2d7e6f4056f73
JensenMPillay <jensen_pillay@hotmail.fr>
2024-02-24 02:43:58 +0100

    [Features] Handle InitBooking

--- src/app/search/components/CarSheet.tsx
+++ src/app/search/components/CarSheet.tsx
@@ -1,24 +1,37 @@
 "use client";
 import CarCard from "@/src/components/CarCard";
-import { buttonVariants } from "@/src/components/ui/button";
+import { Button } from "@/src/components/ui/button";
 import { Card } from "@/src/components/ui/card";
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/src/components/ui/dialog";
+import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetDescription,
   SheetFooter,
   SheetHeader,
   SheetTitle,
 } from "@/src/components/ui/sheet";
-import { cn } from "@/src/lib/utils";
+import { INIT_BOOKING_MUTATION } from "@/src/lib/graphql/booking";
+import { GET_AVAILABLE_CARS_QUERY } from "@/src/lib/graphql/car";
+import { showErrorNotif, showNotif } from "@/src/lib/notifications/toasters";
 import useSearchStore from "@/src/store/useSearchStore";
 import useStore from "@/src/store/useStore";
-import Link from "next/link";
+import { useMutation } from "@apollo/client";
+import { format } from "date-fns";
+import { useState } from "react";
 
 const CarSheet = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
   // Cars Store
-  const { setCarSelected } = useSearchStore();
+  const { setCarSelected, setCars } = useSearchStore();
 
   // Access to Store Data after Rendering (SSR Behavior)
   const carSelected = useStore(useSearchStore, (state) => state.carSelected);
@@ -26,6 +39,42 @@ const CarSheet = () => {
     useSearchStore,
     (state) => state.searchValues?.formValues,
   );
+
+  // Mutation
+  const [initBooking] = useMutation(INIT_BOOKING_MUTATION, {
+    onCompleted: (data) => {
+      setOpen(true);
+    },
+    onError: (error) => {
+      showErrorNotif({
+        description: error.message,
+      });
+      console.error("Mutation Error : ", error);
+    },
+    // Update Cars on Map
+    refetchQueries: [GET_AVAILABLE_CARS_QUERY],
+  });
+
+  // onSubmit Callback
+  const onSubmit = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    event?.preventDefault();
+    showNotif({
+      description: "Submitting your request, please wait...",
+    });
+    try {
+      await initBooking({
+        variables: {
+          startDate: formValues?.date.from,
+          endDate: formValues?.date.to,
+          carId: carSelected?.id,
+        },
+      });
+    } catch (error) {
+      console.error(`Error : ${error}`);
+    }
+  };
 
   return (
     <Sheet open={!!carSelected} onOpenChange={() => setCarSelected(null)}>
@@ -40,22 +89,39 @@ const CarSheet = () => {
         {carSelected && <CarCard car={carSelected} />}
         {formValues && (
           <Card className="text-xs">
-            <p className="text-center">{formValues.location.description}</p>
+            <p className="text-center">
+              {carSelected?.location.formatted_address}
+            </p>
             <p className="flex flex-row justify-around">
-              <span>{formValues.date.from.toDateString()}</span>
+              <span>{format(formValues.date.from, "PPP")}</span>
               <span>-</span>
-              <span>{formValues.date.to.toDateString()}</span>
+              <span>{format(formValues.date.to, "PPP")}</span>
             </p>
             <p></p>
           </Card>
         )}
         <SheetFooter>
-          <SheetClose asChild>
-            <Link href="/booking" className={cn(buttonVariants())}>
-              Book
-            </Link>
-          </SheetClose>
+          <Button
+            size="default"
+            variant="default"
+            onClick={(event) => onSubmit(event)}
+          >
+            Book
+          </Button>
         </SheetFooter>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogContent className="">
+            <DialogHeader>
+              <DialogTitle>Checkout</DialogTitle>
+              <DialogDescription>
+                Choose What Works Best for You!
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button type="submit">Pay</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </SheetContent>
     </Sheet>
   );
